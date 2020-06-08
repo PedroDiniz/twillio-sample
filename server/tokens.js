b7136664ed24569af79c5dfdbb49bf7d57737dcc
PedroDiniz <pedro_nork@hotmail.com>
2020-06-08 16:21:38 -0300

    rooms

--- server/tokens.js
+++ server/tokens.js
@@ -1,6 +1,6 @@
-const twilio = require("twilio");
+const twilio = require('twilio');
 const AccessToken = twilio.jwt.AccessToken;
-const { ChatGrant, VideoGrant, VoiceGrant } = AccessToken;
+const { VideoGrant } = AccessToken;
 
 const generateToken = config => {
   return new AccessToken(
@@ -10,19 +10,9 @@ const generateToken = config => {
   );
 };
 
-const chatToken = (identity, config) => {
-  const chatGrant = new ChatGrant({
-    serviceSid: config.twilio.chatService
-  });
-  const token = generateToken(config);
-  token.addGrant(chatGrant);
-  token.identity = identity;
-  return token;
-};
-
 const videoToken = (identity, room, config) => {
   let videoGrant;
-  if (typeof room !== "undefined") {
+  if (typeof room !== 'undefined') {
     videoGrant = new VideoGrant({ room });
   } else {
     videoGrant = new VideoGrant();
@@ -33,22 +23,4 @@ const videoToken = (identity, room, config) => {
   return token;
 };
 
-const voiceToken = (identity, config) => {
-  let voiceGrant;
-  if (typeof config.twilio.outgoingApplicationSid !== "undefined") {
-    voiceGrant = new VoiceGrant({
-      outgoingApplicationSid: config.twilio.outgoingApplicationSid,
-      incomingAllow: config.twilio.incomingAllow
-    });
-  } else {
-    voiceGrant = new VoiceGrant({
-      incomingAllow: config.twilio.incomingAllow
-    });
-  }
-  const token = generateToken(config);
-  token.addGrant(voiceGrant);
-  token.identity = identity;
-  return token;
-};
-
-module.exports = { chatToken, videoToken, voiceToken };
+module.exports = { videoToken };
--- src/App.js
+++ src/App.js
@@ -4,17 +4,23 @@ import VideoChat from './VideoChat';
 
 const App = () => {
   return (
-      <div className="app">
-        <header>
-          <h1>Video Chat with Hooks</h1>
-        </header>
-        <main>
-          <VideoChat />
-        </main>
-        {/*<footer>*/}
-        {/*  */}
-        {/*</footer>*/}
-      </div>
+    <div className="app">
+      <header>
+        <h1>Video Chat with Hooks</h1>
+      </header>
+      <main>
+        <VideoChat />
+      </main>
+      <footer>
+        <p>
+          Made with{' '}
+          <span role="img" aria-label="React">
+            ⚛️
+          </span>{' '}
+          by <a href="https://twitter.com/philnash">philnash</a>
+        </p>
+      </footer>
+    </div>
   );
 };
 
